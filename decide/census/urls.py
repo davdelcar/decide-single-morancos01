ba0a6d18c538d9568e547f8f3ef6986790a7abdc
lemonada02 <jrlimon02@gmail.com>
2023-12-12 19:40:53 +0100

    fix: eliminar codigo extra y simplificar

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('export/', views.export_csv),
-    path('import/', views.import_csv),
+    path('export/', views.export_xlsx, name='export'),
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
 ]
