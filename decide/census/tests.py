21fd90626f31a1ec15d9888c9c64ec5659e4d126
lemonada02 <jrlimon02@gmail.com>
2023-12-14 10:06:19 +0100

    feat: primeros test creados

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -49,42 +49,42 @@ class CensusTestCase(BaseTestCase):
         self.assertEqual(response.json(), 'Valid voter')
 
     def test_list_voting(self):
-        response = self.client.get('/census/?voting_id={}'.format(1), format='json')
+        response = self.client.get('/census/create/?voting_id={}'.format(1), format='json')
         self.assertEqual(response.status_code, 401)
 
         self.login(user='noadmin')
-        response = self.client.get('/census/?voting_id={}'.format(1), format='json')
+        response = self.client.get('/census/create/?voting_id={}'.format(1), format='json')
         self.assertEqual(response.status_code, 403)
 
         self.login()
-        response = self.client.get('/census/?voting_id={}'.format(1), format='json')
+        response = self.client.get('/census/create/?voting_id={}'.format(1), format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {'voters': [1]})
 
     def test_add_new_voters_conflict(self):
         data = {'voting_id': 1, 'voters': [1]}
-        response = self.client.post('/census/', data, format='json')
+        response = self.client.post('/census/create/', data, format='json')
         self.assertEqual(response.status_code, 401)
 
         self.login(user='noadmin')
-        response = self.client.post('/census/', data, format='json')
+        response = self.client.post('/census/create/', data, format='json')
         self.assertEqual(response.status_code, 403)
 
         self.login()
-        response = self.client.post('/census/', data, format='json')
+        response = self.client.post('/census/create/', data, format='json')
         self.assertEqual(response.status_code, 409)
 
     def test_add_new_voters(self):
         data = {'voting_id': 2, 'voters': [1,2,3,4]}
-        response = self.client.post('/census/', data, format='json')
+        response = self.client.post('/census/create/', data, format='json')
         self.assertEqual(response.status_code, 401)
 
         self.login(user='noadmin')
-        response = self.client.post('/census/', data, format='json')
+        response = self.client.post('/census/create/', data, format='json')
         self.assertEqual(response.status_code, 403)
 
         self.login()
-        response = self.client.post('/census/', data, format='json')
+        response = self.client.post('/census/create/', data, format='json')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(len(data.get('voters')), Census.objects.count() - 1)
 
@@ -249,3 +249,19 @@ class ExportCsvTestCase(TestCase):
 
         expected_data = b'id,voting_id,voter_id\r\n1,2,2\r\n2,2,1\r\n'
         self.assertEqual(response.content, expected_data)
+
+class CensusListTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+
+    def test_census_list(self):
+        response = self.client.get('/census/', format='html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_census_export(self):
+        response = self.client.get('/census/export/', format='html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_census_import(self):
+        response = self.client.get('/census/import/', format='html')
+        self.assertEqual(response.status_code, 200)
