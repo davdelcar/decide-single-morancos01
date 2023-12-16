c3770b012d040156856a9360f8c1526cf484bf2a
davdelcar <davdelcar@alum.us.es>
2023-12-16 15:28:48 +0100

    fix: Admin styles

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -16,6 +16,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -188,10 +189,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-ADMIN_MEDIA_PREFIX = '/admin/media/'
+STATICFILES_DIRS = os.path.join(PROJECT_PATH, 'static'),
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
