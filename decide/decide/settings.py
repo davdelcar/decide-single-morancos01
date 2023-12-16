d3c6d474522bbbc56048838897e0fcdc53b68451
davdelcar <davdelcar@alum.us.es>
2023-12-16 14:21:53 +0100

    fix: Static files vercel

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -156,14 +156,11 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 ADMIN_MEDIA_PREFIX = '/admin/media/'
