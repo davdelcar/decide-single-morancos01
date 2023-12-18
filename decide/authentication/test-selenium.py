dbac699227f1468a40ec782eefaed9244acd0fd2
angmunpri <angmunpri@alum.us.es>
2023-12-18 18:49:54 +0100

    fix: Arreglo de test de distintas tareas #79

---
+++ decide/authentication/test-selenium.py
@@ -0,0 +1,35 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from django.contrib.auth.models import User
+import time
+from selenium.common.exceptions import TimeoutException
+
+class WelcomeLoginTests(StaticLiveServerTestCase):
+    def setUp(self):
+        # Configuración de Selenium
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options)
+        super().setUp()
+
+    def tearDown(self):
+        self.driver.quit()
+        super().tearDown()
+
+    def test_welcome_page_loads(self):
+
+        self.driver.get(f"{self.live_server_url}")
+
+        self.assertIn("Decide", self.driver.title)
+        self.assertIn("Welcome to Decide", self.driver.page_source)
+
+        ir_a_censos_button = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//button[contains(text(), 'Go to Census')]"))
+        )
+        self.assertTrue(ir_a_censos_button.is_displayed())
+
+    
+
---
+++ decide/visualizer/static/visualizer/chart-scripts.js
@@ -0,0 +1,65 @@
+/*Este código se utiliza para el visualizado de gráficas en visualizer.html*/
+
+function createBarChart(ctx, labels, data) {
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Puntuación',
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Resultados de Votación (Gráfico de Barras)'
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Puntuación'
+                    }
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        }
+    });
+}
+
+function createPieChart(ctx, labels, data) {
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Resultados de Votación (Gráfico de Tarta)'
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        }
+    });
+}
